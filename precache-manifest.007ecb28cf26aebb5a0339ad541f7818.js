baff17ec7d03230a605bc9861b21b71be86350fe
sukma rangga <sukmarangga@192.168.1.7>
2022-05-11 08:24:41 +0800

    Updates

---
+++ precache-manifest.007ecb28cf26aebb5a0339ad541f7818.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6577e4f95c61356ab637ffd6ee7aaeba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40c392d5be8712aa730a",
+    "url": "/static/css/main.95d9390f.chunk.css"
+  },
+  {
+    "revision": "e8e740d87ff68cea6b4f",
+    "url": "/static/js/0.b75702e5.chunk.js"
+  },
+  {
+    "revision": "768d435b636c0ae2b481",
+    "url": "/static/js/3.a1c7b7d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/3.a1c7b7d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8693441e4d9ac2bfc88d",
+    "url": "/static/js/4.7e5a2677.chunk.js"
+  },
+  {
+    "revision": "753ded2c32344389515d",
+    "url": "/static/js/5.e724bbf0.chunk.js"
+  },
+  {
+    "revision": "2685d1d042a96409831b",
+    "url": "/static/js/6.da2065d4.chunk.js"
+  },
+  {
+    "revision": "b26a9e817e841e5d385e",
+    "url": "/static/js/7.99913798.chunk.js"
+  },
+  {
+    "revision": "40c392d5be8712aa730a",
+    "url": "/static/js/main.367f6656.chunk.js"
+  },
+  {
+    "revision": "04f630f0f2fd3275e6e7",
+    "url": "/static/js/runtime-main.988c1bb5.js"
+  },
+  {
+    "revision": "5b62e07a8bab762e3dfe1726e493c36e",
+    "url": "/static/media/CoffeeMe.5b62e07a.jpg"
+  },
+  {
+    "revision": "123bd817ff90115f806707f96a80f6f3",
+    "url": "/static/media/Minimum.123bd817.jpeg"
+  },
+  {
+    "revision": "d20480a8cc6a0713107867b4a98775d9",
+    "url": "/static/media/Spin-1s-200px.d20480a8.svg"
+  },
+  {
+    "revision": "29d4c8c4a21e7b1f44227f0bb193316c",
+    "url": "/static/media/aboutMe.29d4c8c4.webm"
+  },
+  {
+    "revision": "cee5288a833e354ee0a2c7b8b92ed51a",
+    "url": "/static/media/batu-gunting-kertas.cee5288a.jpeg"
+  },
+  {
+    "revision": "639565b23ec8ee8b6131f7bd86adb5f5",
+    "url": "/static/media/ingetIn.639565b2.jpg"
+  },
+  {
+    "revision": "fad7c623f75d89f23dce2400833eeb79",
+    "url": "/static/media/no_image.fad7c623.svg"
+  },
+  {
+    "revision": "be6f8cccbac634d32d20f03af7b97aff",
+    "url": "/static/media/password-manager.be6f8ccc.jpeg"
+  },
+  {
+    "revision": "e3fff2ea800161627adcaa79c3b2a38a",
+    "url": "/static/media/rumah-tani.e3fff2ea.jpeg"
+  },
+  {
+    "revision": "6e8d0daf26deea3b51bbd780d1254b9b",
+    "url": "/static/media/social_media_logo_facebook_icon-icons.com_59059.6e8d0daf.svg"
+  },
+  {
+    "revision": "edacb7b7283f39a614611a5657f2ebd3",
+    "url": "/static/media/social_media_logo_instagram_icon-icons.com_59062.edacb7b7.svg"
+  },
+  {
+    "revision": "5562d8a9ee1c1c49f2d432a8530168e3",
+    "url": "/static/media/social_media_logo_linkedin_icon-icons.com_59079.5562d8a9.svg"
+  },
+  {
+    "revision": "964d6fc4aa29e5bff30ef256074f58b4",
+    "url": "/static/media/social_media_logo_whatsapp_icon-icons.com_59066.964d6fc4.svg"
+  },
+  {
+    "revision": "3e65ecf847c1e4ec3888343ada133375",
+    "url": "/static/media/social_media_logo_you_tube_icon-icons.com_59065.3e65ecf8.svg"
+  },
+  {
+    "revision": "7e1d3314219219330e6751479a48ef1b",
+    "url": "/static/media/sumberalam.7e1d3314.jpeg"
+  },
+  {
+    "revision": "5456fc6778cd3cfcc0736ccfd9d29e87",
+    "url": "/static/media/undanganku.5456fc67.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5db859626cef66d6c828b20b5df5ce67.js"
+  "/precache-manifest.007ecb28cf26aebb5a0339ad541f7818.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
